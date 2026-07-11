97b6314ad3ca741ca24245e919f0d3e5147f8791
Sreang Rathanak <sreangrathanak@yahoo.com>
2026-07-11 20:47:01 +0700

    Initial Commit

---
+++ Khmerlang-Keyboard-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Khmerlang-Keyboard-IOS
+//
+//  Created by Sreang Rathanak on 11/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Khmerlang-Keyboard-IOS/Khmerlang_Keyboard_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Khmerlang_Keyboard_IOSApp.swift
+//  Khmerlang-Keyboard-IOS
+//
+//  Created by Sreang Rathanak on 11/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Khmerlang_Keyboard_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
